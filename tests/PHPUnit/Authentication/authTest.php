6add39219f140cea61cfcf778837af9d05681369
Eric Mann <eric@eamann.com>
2014-10-03 15:52:39 -0700

    Set up PHPUnit and test suite

---
+++ tests/PHPUnit/Authentication/authTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace TenUp\Doze;
+
+/**
+ * Class AuthTest
+ *
+ * @package TenUp\Doze
+ *
+ * @group auth
+ */
+class AuthTest extends TestCase {
+	public function test_valid_http_basic() {
+		$this->markTestIncomplete();
+	}
+}
